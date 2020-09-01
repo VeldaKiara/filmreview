9322deb02bd993e5eb2a43a76388910ec3ce4bed
Velda Kiara <veldakarimi@gmail.com>
2020-09-01 22:48:31 +0300

    ✨ added cutomuser to accounts  model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    
+    
+    class Meta:
+        pass
+    def main__str__(self):
+        return self.username
+    
